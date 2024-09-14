1523e909ff454023e9cd100956b97e4f671dfa19
Dmitry Tsutskov <tsutskov6@gmail.com>
2024-09-14 15:05:29 -0400

    Solved html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import hiraganaImage from "./images/hiragana.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,11 +9,57 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <div>
+                <h1>Hello World</h1>
+                <img src={hiraganaImage} alt="ひらがな" />
+            </div>
+            <div>
+                Unordered List:
+                <ul>
+                    <li>あ</li>
+                    <li>い</li>
+                    <li>う</li>
+                </ul>
+            </div>
+            <div>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Dmitrii Tsutskov
-                Hello World
+                automatically reload. Dmitrii Tsutskov Hello World, this is my
+                first application
             </p>
+
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "200px",
+                                    backgroundColor: "red",
+                                }}
+                            />
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "200px",
+                                    backgroundColor: "red",
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
