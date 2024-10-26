a179ca96022fda73c31b96162f65bd3a0477ee22
Greg Nelson <gregory.nelson@maine.edu>
2024-10-26 16:43:55 -0400

    Remove removed problems from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -18,13 +16,9 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
