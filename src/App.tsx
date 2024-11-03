c6aa235259aed6b0c04a76f66b152e8332cfdaa8
Greg Nelson <gregory.nelson@maine.edu>
2024-11-03 18:53:11 -0500

    reduce tasks

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,8 @@ import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
 import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): React.JSX.Element {
     return (
@@ -25,11 +23,7 @@ function App(): React.JSX.Element {
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
             <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
             <hr></hr>
             <MultipleChoiceQuestion
                 options={["a", "b", "c"]}
--- src/form-components/ChangeColor.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export function ChangeColor(): React.JSX.Element {
-    return (
-        <div>
-            <h3>Change Color</h3>
-        </div>
-    );
-}
