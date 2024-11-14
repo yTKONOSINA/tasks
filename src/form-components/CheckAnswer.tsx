5f0be6db9eaa8268a236a11ab4a4c284f38e05b0
Dmitrii Tsutskov <tsutskov6@gmail.com>
2024-11-13 20:53:33 -0500

    solved forms

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,20 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="text"
+                value={userAnswer}
+                onChange={(e) => {
+                    setUserAnswer(e.target.value);
+                }}
+                placeholder="Enter your answer"
+            />
+            <div>{userAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedChoice, setSelectedChoice] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select
+                value={selectedChoice}
+                onChange={(e) => {
+                    setSelectedChoice(e.target.value);
+                }}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            <div>{selectedChoice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
